bb88de6ccce83132e1a08b8cecc5a0a16cda9100
Guilherme de Araujo <gui93araujo@gmail.com>
2026-04-02 23:27:50 -0300

    feat: add Home page initial structure with TailwindCSS

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,20 @@
 export function Home() {
   return (
-    <div className="w-full h-screen bg-neutral-900">
-      <h1 className="text-white text-2xl">Home</h1>
+    <div className="flex flex-col w-full py-4 items-center justify-center">
+      <h1 className="md:text-4xl text-3xl text-white font-bold mt-20">
+        Guilherme Araujo
+      </h1>
+      <span className="text-gray-50 mb-5 mt-4">Veja meus links 👇</span>
+
+      <main className="flex flex-col w-11/12 max-w-xl text-center">
+        <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
+          <a>
+            <p className="text-base md:text-lg">Canal</p>
+          </a>
+        </section>
+
+        <footer className="flex justify-center gap-3 my-4"></footer>
+      </main>
     </div>
   );
 }
