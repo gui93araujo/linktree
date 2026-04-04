2eda6a2e10618bf07bbe8755314e5639674da938
Guilherme de Araujo <gui93araujo@gmail.com>
2026-04-03 21:33:03 -0300

    feat: implemented login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,18 @@
 import { Link } from "react-router-dom";
+import { Input } from "../../components/Input";
+import { useState, FormEvent } from "react";
 
 export function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    console.log("Email:", email);
+    console.log("Password:", password);
+  }
+
   return (
     <div className="flex w-full h-screen items-center justify-center flex-col">
       <Link to="/">
@@ -11,6 +23,30 @@ export function Login() {
           </span>
         </h1>
       </Link>
+      <form
+        className="w-full max-w-xl flex flex-col px-2"
+        onSubmit={handleSubmit}
+      >
+        <Input
+          placeholder="Digite seu email..."
+          value={email}
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <Input
+          placeholder="**********"
+          value={password}
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="h-9 bg-blue-600 rounded border-0 text-lg font-medium text-white cursor-pointer"
+        >
+          Acessar
+        </button>
+      </form>
     </div>
   );
 }
