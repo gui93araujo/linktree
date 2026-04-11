2ae8e121d62f1feab87c2828bf2e04f5e2a17a73
Guilherme de Araujo <gui93araujo@gmail.com>
2026-04-11 13:43:26 -0300

    feat: adjusted home and import

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { Input } from "../../components/Input";
-import { useState, FormEvent } from "react";
+import { useState, type FormEvent } from "react";
 
 import { auth } from "../../services/firebaseConnection";
 import { signInWithEmailAndPassword } from "firebase/auth";
